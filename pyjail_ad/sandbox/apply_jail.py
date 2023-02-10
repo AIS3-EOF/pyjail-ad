ce9bef6efd7135d234eb619a5895e330ced7e2b5
maple3142 <kirby741852963@gmail.com>
2023-02-10 17:28:10 +0800

    increase max_more_code

--- pyjail_ad/sandbox/apply_jail.py
+++ pyjail_ad/sandbox/apply_jail.py
@@ -7,13 +7,15 @@ import sys, json
 print(json.dumps(jail(sys.stdin.read())))
 """
 
-DEFAULT_JAIL = """from typing import Tuple
+MAX_MORE_CODE = 75
+
+DEFAULT_JAIL = f"""from typing import Tuple
 
 def jail(code: str) -> Tuple[bool, str]:
     '''
     This function should return a tuple of two values:
     - first value is a boolean indicating whether the code should be run or not
-    - second value is the modified the code, but it must satisfy len(new_code) <= len(code) + 50 or your jail will be ignored
+    - second value is the modified the code, but it must satisfy len(new_code) <= len(code) + {MAX_MORE_CODE} or your jail will be ignored
 
     You also shouldn't print anything to stdout here.
     '''
@@ -25,7 +27,7 @@ def apply_jail(jail: str, code: str) -> bool:
     try:
         r = run(JAIL_TEMPLATE.format(jail), stdin=code)
         allow, new_code = json.loads(r.stdout.strip())
-        if len(new_code) > len(code) + 50:
+        if len(new_code) > len(code) + MAX_MORE_CODE:
             return True, code
         return allow, new_code
     except:
